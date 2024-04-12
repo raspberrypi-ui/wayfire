feae7daeed1a9c4be9e8974104aea653a34d131d
Simon Long <simon@raspberrypi.com>
2024-04-12 17:11:23 +0100

    Fix for crash after multiple Xwayland windows opened

--- plugins/pixdecor/deco-theme.cpp
+++ plugins/pixdecor/deco-theme.cpp
@@ -46,6 +46,7 @@ decoration_theme_t::~decoration_theme_t()
     wl_event_source_remove (evsrc);
     inotify_rm_watch (inotify_fd, wd_cfg_file);
     inotify_rm_watch (inotify_fd, wd_cfg_dir);
+    close (inotify_fd);
 }
 
 void decoration_theme_t::update_colours (void)
